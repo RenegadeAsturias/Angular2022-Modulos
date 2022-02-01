38cf60bb0f958bde2dfa0f31f0e4ae8f6b2bc366
Renegade Asturias <renegade.asturias@gmail.com>
2022-02-01 01:18:13 +0100

    Proyecto Angular con modulos

---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'app-empleados-component',
+    templateUrl: './empleados.component.html',
+    styleUrls: ['./empleados.component.css']
+})
+
+export class EmpleadosComponent {
+
+}
